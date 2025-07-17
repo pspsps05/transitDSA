0fcf09adf66023cbb525c1b622a4a58f84d2ac5c
yeetenglee <yeeteng0309@gmail.com>
2025-07-17 16:38:09 +0800

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int graph02  =  {{0,2},
+                    {2,0,3},
+                    {0,3,0,2},
+                    {0,0,2,0,2},
+                    {0,0,0,2,0,2},
+                    {0,0,0,0,2,0,2},
+                    {0,0,0,0,0,2,0,1},
+                    {0,0,0,0,0,0,1,0,2},
+                    {0,0,0,0,0,0,0,2,0,3},
+                    {0,0,0,0,0,0,0,0,3,0,2},
+                    {0,0,0,0,0,0,0,0,0,2,0,2},
+                    {0,0,0,0,0,0,0,0,0,0,2,0,2},
+                    {0,0,0,0,0,0,0,0,0,0,0,2,0},};
+    string line02[13] = {{"Jelatek"},{"Dato' Keramat"},{"Damia"},{"Ampang Park"},{"KLCC"}
+                        {"Kampung Baru"},{"Dang Wangi"},{"Masjid Jamek"},{"Pasar Seni"},{"KL Sentral"}
+                        {"Bangsar"},{"Abdullah Hukum"},{"Kerinchi"}};
+    
+}
